d0ad7a31d4ead080129d8c63d9b0ceb120a8457b
neeraj <ndhankhola@isystango.com>
2020-10-08 19:54:09 +0530

    removed unused repository method

--- src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -13,7 +13,4 @@ public interface OrderRepository extends JpaRepository<Order, Long>{
 	
 	@Query(value = "select p.id as productId, p.name as productName, p.description as productDescription, po.product_price as productPrice, o.created_at as purchaseDate from orders o inner join product_order po on o.id = po.order_id inner join product p on po.product_id = p.id where (date(o.created_at) between :startDate and :endDate) and o.user_id = :userId", nativeQuery = true)
 	List<ProductDetail> getOrdersBetweenDates(Long userId, Date startDate, Date endDate);
-	
-	@Query(value = "select sum(po.product_price) from orders o inner join product_order po on o.id = po.order_id inner join product p on po.product_id = p.id where (date(o.created_at) between :startDate and :endDate) and o.user_id = :userId", nativeQuery = true)
-	Float getTotalPriceOfOrdersBetweenDates(Long userId, Date startDate, Date endDate);
 }
